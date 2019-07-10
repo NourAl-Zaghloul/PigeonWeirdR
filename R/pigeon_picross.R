5aebd15792ff8543a316511208b52229dd815094
NourAl-Zaghloul <nzaghlou@andrew.cmu.edu>
2019-07-10 16:27:53 -0400

    Update pigeon_picross.R
    Creates a randomized picross puzzle, but no clues.
    
    TODO:
     * picross_clues
     * make it playable
     * Have a data.set of picross-able images (already set up)

--- R/pigeon_picross.R
+++ R/pigeon_picross.R
@@ -1,1 +1,7 @@
-pigeon_picross <- function(){}
+pigeon_picross <- function(x = 10, y = 10, seed = 112211, locations = .5){
+  picross_fill <- c(rep(1, trunc(x*y*locations)), rep(0, x*y - trunc(x*y*locations) ))
+  set.seed(seed)
+  picross_fill <- sample(picross_fill)
+  picross_inner <- matrix(picross_fill, nrow = y, ncol = x)
+  return(picross_inner)
+}
