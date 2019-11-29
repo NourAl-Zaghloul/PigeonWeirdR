646e8762873089219ae683ae456d477a2079bdda
Nour al-Zaghloul <Nouralzaghloul@gmail.com>
2019-11-28 22:19:55 -0500

    Saved Pigeon Data + Returns random pigeon (simple)

--- R/pigeon_pigeon.R
+++ R/pigeon_pigeon.R
@@ -1,4 +1,10 @@
-pigeon_pigeon <- function(){}
+pigeon_pigeon <- function(){
+  pigeon_species <- readRDS("pigeon_species.RDS")
+  pigeon_breeds <- readRDS("pigeon_breeds.RDS")
+
+  return(pigeon_species$`Common name`[trunc(runif(1,min = 1, max = 100))])
+
+}
 
 
 #### Goals ------
@@ -6,3 +12,16 @@ pigeon_pigeon <- function(){}
 # To Make a package that returns random pigeons
 # Also, adjective phrases with pigeons
 # Sentences?
+
+#### Getting the data ----
+# library(rvest)
+# library(tidyverse)
+#
+# pigeon_html <- read_html("https://en.wikipedia.org/wiki/List_of_wild_pigeon_species")
+# pigeon_data_species <- html_table(pigeon_html, fill = TRUE)[[1]]
+# saveRDS(pigeon_data_species, file = "pigeon_species.RDS")
+#
+# pigeon2_html <- read_html("https://en.wikipedia.org/wiki/List_of_pigeon_breeds")
+# pigeon_data_breed <- html_nodes(pigeon2_html, css = "ul li") %>% html_text()
+# pigeon_data_breed <- pigeon_data_breed[27:750]
+# saveRDS(pigeon_data_breed, file = "pigeon_breeds.RDS")
